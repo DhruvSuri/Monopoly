ca2f6a9907efb72b489debdb3679635c3b58ce2a
Dhruv Suri <dhruvsuri@Dhruvs-MacBook-Pro.local>
2018-11-11 12:01:04 -0500

    Added Board config to Board

--- Board.py
+++ Board.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python
 import Constant as constant
+import json
 
 class Board:
 		
 	def __init__(self):
 		print('Init of Board')
 		self.board = {}
+		self.board_config = self.initialize_board_config()
 		self.board[constant.IN_JAIL_INDEX] = {}
 		for i in range(constant.BOARD_SIZE):
 			self.board[i] = {}
+
+	def initialize_board_config(self):
+		with open('BoardConfig.json', 'r') as f:
+			config = json.load(f)
+		# print(config)
+		return config
