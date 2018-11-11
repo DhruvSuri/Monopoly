7a1cbf689b4998a617e384e9a1d6aa947b7edd50
Dhruv Suri <dhruvsuri@Dhruvs-MacBook-Pro.local>
2018-11-11 12:25:44 -0500

    Added dummy Player methods

--- Player.py
+++ Player.py
@@ -5,3 +5,23 @@ class Player:
 	def __init__(self):
 		logger = logging.getLogger("com.sbu.monopoly.player")
 		logger.info("Player initialized")
+
+	def getBMSTDecision(self, state):
+		return action	
+
+	def respondTrade(self, state):
+		return action
+	
+	def buyProperty(self, state):
+		return True
+	
+	def auctionProperty(self, state):
+		return False
+	
+	def respondMortgage(self, state):
+		return True
+
+	def jailDecision(self, state):
+		return ("P")
+	
+	
